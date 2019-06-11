eeda519a191914c4d40016473c75d000093de54f
Chris Stylianou <chris5287@gmail.com>
2019-06-11 21:34:18 +0100

    Added Azure Client with support for FileFetcher

---
+++ common/lib/dependabot/clients/azure.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+require "dependabot/shared_helpers"
+require "excon"
+
+module Dependabot
+  module Clients
+    class Azure
+      class NotFound < StandardError; end
+
+      #######################
+      # Constructor methods #
+      #######################
+
+      def self.for_source(source:, credentials:)
+        credential =
+          credentials.
+          select { |cred| cred["type"] == "git_source" }.
+          find { |cred| cred["host"] == source.hostname }
+
+        new(source, credential)
+      end
+
+      ##########
+      # Client #
+      ##########
+
+      def initialize(source, credentials)
+        @source = source
+        @credentials = credentials
+      end
+
+      def fetch_commit(_repo, branch)
+        response = get(source.api_endpoint +
+          source.organization + "/" + source.project +
+          "/_apis/git/repositories/" + source.unscoped_repo +
+          "/stats/branches?name=" + branch)
+
+        JSON.parse(response.body).fetch("commit").fetch("commitId")
+      end
+
+      def fetch_default_branch(_repo)
+        response = get(source.api_endpoint +
+          source.organization + "/" + source.project +
+          "/_apis/git/repositories/" + source.unscoped_repo)
+
+        JSON.parse(response.body).fetch("defaultBranch").gsub("refs/heads/", "")
+      end
+
+      def fetch_repo_contents(_repo, commit = nil, path = nil)
+        actual_path = path
+        if path.to_s.empty?
+          actual_path = "/"
+        end
+
+        tree_url = source.api_endpoint +
+          source.organization + "/" + source.project +
+          "/_apis/git/repositories/" + source.unscoped_repo +
+          "/items?path=" + actual_path
+
+        if !commit.to_s.empty?
+          tree_url += "&versionDescriptor.versionType=commit&versionDescriptor.version=" + commit
+        end
+
+        tree_response = get(tree_url)
+
+        tree = JSON.parse(tree_response.body).fetch("objectId")
+
+        response = get(source.api_endpoint +
+          source.organization + "/" + source.project +
+          "/_apis/git/repositories/" + source.unscoped_repo +
+          "/trees/" + tree + "?recursive=false")
+
+        JSON.parse(response.body).fetch("treeEntries")
+      end
+
+      def fetch_file_contents(_repo, commit, path)
+        response = get(source.api_endpoint +
+          source.organization + "/" + source.project +
+          "/_apis/git/repositories/" + source.unscoped_repo +
+          "/items?path=" + path + "&versionDescriptor.versionType=commit&versionDescriptor.version=" + commit)
+
+        response.body
+      end
+
+      def get(url)
+        response = Excon.get(
+          url,
+          user: credentials&.fetch("username"),
+          password: credentials&.fetch("password"),
+          idempotent: true,
+          **SharedHelpers.excon_defaults
+        )
+        raise NotFound if response.status == 404
+
+        response
+      end
+
+      private
+
+      attr_reader :credentials
+      attr_reader :source
+    end
+  end
+end
